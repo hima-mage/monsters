d52525d25b186e63a3698bbd9fc8f9b0d20058ac
isheikh-dev <ishiekh.dev.v1@gmail.com>
2020-04-08 04:16:24 +0200

    Updates

---
+++ precache-manifest.0d56eddd467be03c0fc78c62ddb6afab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e08cfb87664d03fba30e1b59c80a28",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "7e07c42236aa9e8c72c3",
+    "url": "/monsters/static/css/main.6a37ba50.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/monsters/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e07c42236aa9e8c72c3",
+    "url": "/monsters/static/js/main.96aaf437.chunk.js"
+  },
+  {
+    "revision": "28c7cc27b21cfacfd62e",
+    "url": "/monsters/static/js/runtime-main.8d89be33.js"
+  }
+]);
